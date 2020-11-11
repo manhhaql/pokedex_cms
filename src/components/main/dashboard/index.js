3d47bed49fd6ba8b9fc2b18d5b9d0c2222a4b6a7
hamanhnguyen <manhhaql@gmail.com>
2020-11-11 22:10:06 +0700

    temporary dashboard

--- src/components/main/dashboard/index.js
+++ src/components/main/dashboard/index.js
@@ -1,10 +1,101 @@
-const DashboardComponent = () => {
+import React, {useState, useEffect} from "react";
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+import {
+    Row, 
+    Col, 
+    Container, 
+    Label}  from "reactstrap";
+
+import HttpRequest from 'helper/httpRequest';
+
+import './Dashboard.css';
+
+const DashboardComponent = (props) => {
+    const [totalPokemon, setTotalPokemon] = useState();
+    const [totalUser, setTotalUser] = useState();
+    const [totalAbility, setTotalAbility] = useState();
+
+    const getTotalPokemon = () => {
+        HttpRequest.get({
+            url: `pokemon/list`,
+            params:{
+                limit: 1
+            }
+        }).then(response => {
+            return setTotalPokemon(response.data.total)
+        }).catch(error => {
+
+        })
+    }
+    const getTotalUser = () => {
+        HttpRequest.get({
+            url: `users`,
+            token: props.appAuthentication.token,
+            params:{
+                limit: 1
+            }
+        }).then(response => {
+            return setTotalUser(response.data.total)
+        }).catch(error => {
+
+        })
+    }
+    const getTotalAbility = () => {
+        HttpRequest.get({
+            url: `properties/ability`,
+            params:{}
+        }).then(response => {
+            return setTotalAbility(response.data.data.length)
+        }).catch(error => {
+
+        })
+    }
+
+    useEffect(()=>{
+        getTotalPokemon();
+        getTotalUser();
+        getTotalAbility();
+    }, []);
+
     return (
-        <div>
-            <h2>DASHBOARD COMPONENT</h2>
-            <p>Statistics numbers will showed here</p>
+        
+        <div className="Dashboard">
+            <h2>DASHBOARD</h2>
+            <Container>
+                <Row>
+                    <Col className="Dashboard-item bg-success">
+                        <Label>Total Pokemon:</Label>
+                        <h2>{totalPokemon}</h2>
+                    </Col>
+                    <Col className="Dashboard-item bg-primary">
+                        <Label>Total User:</Label>
+                        <h2>{totalUser}</h2>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col className="Dashboard-item bg-warning">
+                    <Label>Total Blog Post:</Label>
+                        <h2>Coming Soon</h2>
+                    </Col>
+                    <Col className="Dashboard-item bg-info">
+                        <Label>Total Ability:</Label>
+                        <h2>{totalAbility}</h2>
+                    </Col>
+                </Row>
+
+            </Container>
         </div>
     )
 };
 
-export default DashboardComponent;
+const mapStateToProps = (state) => ({
+    appAuthentication: state.appAuthentication.current
+});
+
+const mapDispatchToProps = (dispatch) => ({
+
+});
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(DashboardComponent));
--- src/components/main/pokemon/PokemonAbility.js
+++ src/components/main/pokemon/PokemonAbility.js
@@ -2,22 +2,22 @@ import React, { useState } from 'react';
 import { Tooltip } from 'reactstrap';
 
 const PokemonAbilityComponent = (props) => {
-    const [tooltipOpen, setTooltipOpen] = useState(false);
-    const toggle = () => setTooltipOpen(!tooltipOpen);
-    return (
-      <div>
-        <span className="Pokemon-ability__text" id={`Ability-${props.ability.id}`}>
-            {props.ability.name}
-        </span>
-        <Tooltip placement="right" 
-                isOpen={tooltipOpen}
-                target={`Ability-${props.ability.id}`} 
-                toggle={toggle}
-        >
-            {props.ability.description}                    
-        </Tooltip>
-      </div>
-    );
-  }
+  const [tooltipOpen, setTooltipOpen] = useState(false);
+  const toggle = () => setTooltipOpen(!tooltipOpen);
+  return (
+    <div>
+      <span className="Pokemon-ability__text" id={`Ability-${props.ability.id}`}>
+        {props.ability.name}
+      </span>
+      <Tooltip placement="right"
+        isOpen={tooltipOpen}
+        target={`Ability-${props.ability.id}`}
+        toggle={toggle}
+      >
+        {props.ability.description}
+      </Tooltip>
+    </div>
+  );
+}
 
 export default PokemonAbilityComponent;
